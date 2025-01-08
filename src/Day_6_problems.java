8caca325363324371b3e3263327bc6c835c27b9f
ganesh peddisetti <168344926+githubganesh2005@users.noreply.github.com>
2025-01-08 14:20:57 +0530

    Add files via upload

---
+++ src/Day_6_problems.java
@@ -0,0 +1,14 @@
+public class Day_6_problems {
+    public static void main(String[] args) {
+        // nested loop
+        //outer loop for rows
+        for(int i = 0;i < 4; i++){ 
+        //inner loops for columns
+        for(int j = 0; j <= i; j++){ // j = 0 < 4
+            System.out.print("y");
+        }
+        System.out.println();
+        }
+    }
+    
+}

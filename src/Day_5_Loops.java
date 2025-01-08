25b7d552d6b26a6bb019e9ee17744370879122f0
ganesh peddisetti <168344926+githubganesh2005@users.noreply.github.com>
2025-01-08 11:32:11 +0530

    Add files via upload

---
+++ src/Day_5_Loops.java
@@ -0,0 +1,19 @@
+ class Day_5_Loops {
+    public static void main(String[] args) {
+      System.out.println("ganesh");
+      // 10 times
+      // count = 0
+      // count = 1
+      // count public= 2
+
+      // count = 3
+
+      for (int i = 0; i < 10; i++){ //i = 3 <= 10 false
+        System.err.println("ganesh");
+          
+      }
+      String s = "phone";
+      for(int i = 0; i < s.length(); i++){// i<5
+    }
+}       
+
